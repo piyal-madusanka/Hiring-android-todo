c9b8fae9ddf889487ed45250d367789c5fc3594e
Piyal_Madushanka <piyalmadushankaofficial@gmail.com>
2025-08-30 14:59:20 +0530

    New cache list changes added

--- app/src/main/java/com/madushanka/todoapp/MainActivity.kt
+++ app/src/main/java/com/madushanka/todoapp/MainActivity.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.getValue
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -11,8 +14,11 @@ import com.madushanka.rehersal.presentation.screen.TodoListScreen
 import com.madushanka.todoapp.presentation.navigation.SplashScreen
 import com.madushanka.todoapp.presentation.navigation.TodoListScreen
 import com.madushanka.todoapp.presentation.screen.SplashScreen
+import com.madushanka.todoapp.presentation.viewmodel.TodoViewModel
 import com.madushanka.todoapp.ui.theme.MyApplicationTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,12 +39,16 @@ class MainActivity : ComponentActivity() {
                         })
                     }
                     composable<TodoListScreen> {
+
+                        val todoViewModel: TodoViewModel = hiltViewModel()
+                        val todoState by todoViewModel.todoState.collectAsStateWithLifecycle()
+
                         TodoListScreen(
                             navigateToTodoDetails = { index, todo -> },
-                            onEvent = { todoEvent -> },
-                            todoState = com.madushanka.todoapp.presentation.state.TodoState.Success(
-                                emptyList()
-                            )
+                            onEvent = { todoEvent ->
+
+                            },
+                            todoState = todoState
                         )
 
                     }
