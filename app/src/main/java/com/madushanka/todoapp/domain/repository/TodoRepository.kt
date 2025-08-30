66090ba6794869dbc0a16e57baba569a971663ac
Piyal_Madushanka <piyalmadushankaofficial@gmail.com>
2025-08-30 14:27:23 +0530

    todo domain repository added

---
+++ app/src/main/java/com/madushanka/todoapp/domain/repository/TodoRepository.kt
@@ -0,0 +1,12 @@
+package com.madushanka.todoapp.domain.repository
+
+import com.madushanka.todoapp.domain.model.Todo
+import kotlinx.coroutines.flow.Flow
+
+
+interface TodoRepository {
+
+   suspend fun getTodos(): Flow<Result<List<Todo>>>
+
+    suspend fun addTodo(todo: Todo): Flow<Result<Unit>>
+}
